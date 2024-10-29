4396384e34001e1e6125fe0269e70580429464f8
JumaAskar <182446740+JumaAskar@users.noreply.github.com>
2024-10-29 18:11:13 +0600

    Update GradDescent.py

--- GradDescent.py
+++ GradDescent.py
@@ -1,48 +1,57 @@
-import random
-
-def derivative_k (arr1, arr2, n, k, b):
+def derivative_w (x, y, w, b):
     sum_all = 0
-    for i in range(len(arr1)):
-        sum_all += (k * arr1[i] + b - arr2[i]) * arr1[i]
-    der = 2 * sum_all / n
+    for x_i, y_i in zip(x, y):
+        sum_all += (w * x_i + b - y_i) * x_i
+    der = 2 * sum_all / len(x)
     return der
 
-def derivative_b (arr1, arr2, n, k, b):
+
+def derivative_b (x, y, w, b):
     sum_all = 0
-    for i in range(len(arr1)):
-        sum_all += (k * arr1[i] + b - arr2[i])
-    der = 2 * sum_all / n
+    for x_i, y_i in zip(x, y):
+        sum_all += w * x_i + b - y_i
+    der = 2 * sum_all / len(x)
     return der
 
-def MSE (arr1, arr2, n, k, b):
+
+def cost_function(x, y, w, b):
     sum_all = 0
-    for i in range(len(arr1)):
-        sum_all += (arr1[i] * k + b - arr2[i]) ** 2
-    cost_function = sum_all / n
+    for x_i, y_i in zip(x, y):
+        sum_all += (w * x_i + b - y_i) ** 2
+    cost_function = sum_all / len(x)
     return cost_function
 
-lr = 0.00005
-limit = 5000
-n = int(input("Enter the number of points.\n"))
-xs = []
-ys = []
-tem_k = 0
-tem_b = 0
 
-for i in range(0, n):
-    in_put = input("Enter the x & y coordinates of the point {0}.\n".format(i))
-    in_put = in_put.split(" ")
-    xs.append(int(in_put[0]))
-    ys.append(int(in_put[1]))
-    
-k = random.randint(0, 15)
-b = random.randint(0, 100)
-while MSE(xs, ys, n, k, b) > limit:
-    tem_k = k - lr * derivative_k(xs, ys, n, k, b)
-    tem_b = b - lr * derivative_b(xs, ys, n, k, b)
-    k = tem_k
-    b = tem_b
+learning_rate_w = 0.00001
+learning_rate_b = 0.001
 
-print(MSE(xs, ys, n, k, b))
-print(k)
-print(b)
+limit = 100
+
+x = []
+y = []
+
+temp_w = 0
+temp_b = 0
+
+points_number = int(input("Enter the number of points.\n"))
+
+for i in range(1, n + 1):
+    points_input = input("Enter the x & y coordinates of the point {0}.\n".format(i))
+    points_input = points_input.split(" ")
+    x.append(int(points_input[0]))
+    y.append(int(points_input[1]))
+
+w = 0
+b = 0
+
+for _ in range(limit):
+    temp_w = w - learning_rate_w * derivative_w(x, y, w, b)
+    temp_b = b - learning_rate_b * derivative_b(x, y, w, b)
+    w = temp_w
+    b = temp_b
+    # print("w:", w, "| b:", b, "| cost function:", cost_function(x, y, w, b))
+
+print("w:", w)
+print("b:", b)
+print("cost function:", cost_function(x, y, w, b))
+
